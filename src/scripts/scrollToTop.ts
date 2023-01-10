087edd6d1f880f68ca7d43f89a99b42b5cc3cc24
Rodolpheanger <rodolphe.anger@yahoo.fr>
2023-01-10 20:04:49 +0100

    commit fin de journée

--- src/scripts/scrollToTop.ts
+++ src/scripts/scrollToTop.ts
@@ -1,4 +1,9 @@
 export const scrollToTop = (): void => {
   const button = document.querySelector(".scrollToTop") as HTMLDivElement;
+  window.addEventListener("scroll", () => {
+    window.scrollY > 300
+      ? button.classList.add("visible")
+      : button.classList.remove("visible");
+  });
   button.addEventListener("click", () => window.scrollTo(0, 0));
 };
