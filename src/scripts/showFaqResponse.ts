06c9238424bd9bb3b5b8acbaba3136e39a3316c1
Rodolpheanger <rodolphe.anger@yahoo.fr>
2023-01-12 20:20:18 +0100

    commit fin de journée

--- src/scripts/showFaqResponse.ts
+++ src/scripts/showFaqResponse.ts
@@ -1,19 +1,16 @@
-// TODO: voir pour ajouter un eventListener sur chaque question pour ajouter la class visible et faire apparaître la réponse !!!
-
 export const showFaqResponse = (): void => {
   const questions = document.querySelectorAll(
-    ".fa-angles-down"
+    ".fa-solid.fa-angles-down"
   ) as NodeListOf<HTMLParagraphElement>;
-  console.log(questions);
-  questions.forEach((question, index) => {
-    console.log("question: ", question, " index: ", index);
-    question.addEventListener("click", (e) => {
-      console.log(e.target);
-      const response = document.querySelector("question + p");
-      console.log(response);
-      response?.classList.contains("visible")
-        ? response?.classList.remove("visible")
-        : response?.classList.add("visible");
-    });
+
+  questions.forEach((question) => {
+    const response = question.parentElement
+      ?.nextElementSibling as HTMLParagraphElement;
+    response &&
+      response.style.setProperty("--max-height", response.offsetHeight + "px");
+    response &&
+      question.addEventListener("click", () => {
+        response.classList.toggle("visible");
+      });
   });
 };
