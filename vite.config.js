3e82f454fec2dee87c794c7567214cc624f938f4
Rodolpheanger <rodolphe.anger@yahoo.fr>
2023-01-31 14:03:11 +0100

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 // vite.config.js
-import { resolve, __dirname } from "path";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+
+// const _dirname =
+//   typeof __dirname !== "undefined"
+//     ? __dirname
+//     : dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   build: {
